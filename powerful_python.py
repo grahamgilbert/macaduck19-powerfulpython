9722ff0cf5bef19f07bf29bd06d3ad938050142c
Graham Gilbert <graham.gilbert@airbnb.com>
2019-01-27 14:50:33 -0800

    Tests

--- powerful_python.py
+++ powerful_python.py
@@ -2,4 +2,23 @@
 
 import subprocess
 
-print('hello, world')
+
+def requires_reboot(cmd_output):
+    if "restart" in cmd_output.lower():
+        return "True"
+    else:
+        return False
+
+
+def main():
+    cmd = [
+        "/usr/sbin/softwareupdate",
+        "-dla"
+    ]
+    output = subprocess.check_output(cmd)
+    if requires_reboot == True:
+        subprocess.call(["/sbin/reboot"])
+
+
+if __name__ == "__main__":
+    main()
