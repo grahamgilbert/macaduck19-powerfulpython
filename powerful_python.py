a44ada9224d46d32bf535d4dce7490604945c7e8
Graham Gilbert <graham.gilbert@airbnb.com>
2019-02-21 20:28:41 -0800

    Description

--- powerful_python.py
+++ powerful_python.py
@@ -4,6 +4,10 @@ import subprocess
 
 
 def requires_reboot(cmd_output):
+    """
+    Input: output from softwareupdate. Tests if "restart" is in the output.
+    Output: Boolean.
+    """
     if "restart" in cmd_output.lower():
         return "True"
     else:
